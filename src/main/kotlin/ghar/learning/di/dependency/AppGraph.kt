db5bd83b5d2cc7ac240ec1e8e7aa0a8cea5ecb8e
Sayyid A Naqvi <naqvi.sdsu@gmail.com>
2022-09-11 10:36:55 -0500

    added singleton-based approach

--- src/main/kotlin/ghar/learning/di/dependency/AppGraph.kt
+++ src/main/kotlin/ghar/learning/di/dependency/AppGraph.kt
@@ -3,7 +3,12 @@ package ghar.learning.di.dependency
 import dagger.Component
 import ghar.learning.di.dependency.nomudule.House
 import ghar.learning.di.dependency.module.GsonModule
+import javax.inject.Singleton
 
+@Singleton
+/** singleton-dependency forces to annoate component
+ * as singleton
+ * */
 @Component(modules = [GsonModule::class])
 interface AppGraph {
     fun injectHouse(): House
--- src/main/kotlin/ghar/learning/di/dependency/module/GsonModule.kt
+++ src/main/kotlin/ghar/learning/di/dependency/module/GsonModule.kt
@@ -3,11 +3,13 @@ package ghar.learning.di.dependency.module
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class GsonModule {
 
     @Provides
+    @Singleton
     fun injectsGson():Gson {
         return Gson()
     }
